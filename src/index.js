26806382899dd6880ae712e4b3decfca8acd3878
tsatse <joachim.revol-tissot.ext@francetv.fr>
2015-03-22 19:19:02 +0100

    made this actually work

--- src/index.js
+++ src/index.js
@@ -3,22 +3,21 @@ var RSVP = require('rsvp');
 
 function TestsInterface(dataAdapter) {
     this._dataAdapter = dataAdapter;
-    this._events = new events.EventEmitter();
 }
 
 TestsInterface.prototype = {
     _getTagsFromTest: function _getTagsFromTest(test) {
         for(var testTags in test) {}
-        return testTags;
+        return testTags.split(',').map(function(tag) {return tag.trim();});
     },
 
     getTestTags: function getTestTags() {
         var globalTags = {};
 
-        this._dataAdapter.get('tests/library')
+        return this._dataAdapter.get('tests/library')
             .then(function(testLibrary) {
                 testLibrary.forEach(function(test) {
-                    var tags = this._getTagsFromTest()
+                    var tags = this._getTagsFromTest(test);
                     tags.forEach(function(tag) {
                         globalTags[tag] = true;
                     });
